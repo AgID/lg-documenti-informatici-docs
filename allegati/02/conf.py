8efc3d7935979045fb36954d0773705de922cbfa
Walter Arrighetti <walter-arrighetti@users.noreply.github.com>
2019-12-05 01:42:09 +0100

    Create conf.py

---
+++ allegati/02/conf.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+master_doc = 'index'                      # The master toctree document.
+
+#### Options for LaTeX output
+latex_elements = { 'papersize':"a4paper" }
+
+latex_logo = "media/images/AgID_logo.png"  # The name of an image file to place at the top of the title page.
+latex_show_pagerefs = False                # If true, show page references after internal links.
+latex_show_urls = False                    # If true, show URL addresses after external links.
+
+# Documents to append as an Appendix to all manuals.
+latex_appendices = [
+#    "attachments/",
+]
